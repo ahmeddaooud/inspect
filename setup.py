020e40aa1166aba6b5017836607803020cc17114
ahmeddaooud <33223701+ahmeddaooud@users.noreply.github.com>
2018-06-26 02:43:23 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name='PAYFORT Request Inspector',
+    name='PAYFORT-Inspector',
     version='2.0.0',
     author='PAYFORT',
     author_email='adaoud@payfort.com',
