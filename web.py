6e879d3232a11a4deb0316997d9ad41a71d1e52d
ahmeddaooud <33223701+ahmeddaooud@users.noreply.github.com>
2018-06-26 03:54:52 +0300

    Update web.py

--- web.py
+++ web.py
@@ -5,4 +5,4 @@ from inspector import app
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', config.PORT_NUMBER))
-    app.run(host='127.0.0.1', port=port, debug=config.DEBUG)
+    app.run(host='0.0.0.0', port=port, debug=config.DEBUG)
