59bec1b6da5f952f809da07ebc2d903dd3ea9cca
ahmdaoud <adaoud@payfort.com>
2018-07-22 19:52:21 +0300

    Fix error flash for duplicate inspector name

--- inspector/api.py
+++ inspector/api.py
@@ -3,13 +3,12 @@ import urllib2
 
 import yaml
 import operator
-import re
 
-from flask import session, make_response, request, redirect, render_template, flash
+from flask import session, make_response, request, redirect, render_template
 
 from inspector import app, db
-from inspector.util import tinyid
 from inspector.views import expand_recent_bins
+
 
 def _response(object, code=200):
     jsonp = request.args.get('jsonp')
@@ -19,9 +18,9 @@ def _response(object, code=200):
     else:
         resp = make_response(json.dumps(object), code)
         resp.headers['Content-Type'] = 'application/json'
+        resp.headers['Provided-By'] = "https://www.payfort.com"
         resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
-
 
 
 def _safe_form_response(object, code=200):
@@ -30,7 +29,7 @@ def _safe_form_response(object, code=200):
         resp = make_response('%s(%s)' % (jsonp, json.dumps(object)), 200)
         resp.headers['Content-Type'] = 'text/javascript'
     else:
-        #FORMAT FORM
+        # FORMAT FORM
         json_format = json.dumps(object, sort_keys=True, indent=4, separators=(',', ': '))
         json_format = json_format.replace('=', '\": \"')
         json_format = json_format.replace('&', '\", \"')
@@ -42,8 +41,10 @@ def _safe_form_response(object, code=200):
         json_format = '{' + json_format + '}'
         resp = make_response(json_format, code)
         resp.headers['Content-Type'] = 'application/json'
+        resp.headers['Provided-By'] = "https://www.payfort.com"
         resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
+
 
 def _safe_query_response(object, code=200):
     jsonp = request.args.get('jsonp')
@@ -51,13 +52,13 @@ def _safe_query_response(object, code=200):
         resp = make_response('%s(%s)' % (jsonp, json.dumps(object)), 200)
         resp.headers['Content-Type'] = 'text/javascript'
     else:
-        #FORMAT query
+        # FORMAT query
         json_format = json.dumps(object)
         resp = make_response(json_format, code)
         resp.headers['Content-Type'] = 'application/json'
+        resp.headers['Provided-By'] = "https://www.payfort.com"
         resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
-
 
 
 def _safe_json_response(object, code=200):
@@ -66,33 +67,14 @@ def _safe_json_response(object, code=200):
         resp = make_response('%s(%s)' % (jsonp, json.dumps(object)), 200)
         resp.headers['Content-Type'] = 'text/javascript'
     else:
-        #FORMAT JSON
+        # FORMAT JSON
         json_format = json.dumps(object, sort_keys=True, indent=4, separators=(',', ': '))
         json_safe_format = yaml.safe_load(json_format)
         resp = make_response(json_safe_format, code)
         resp.headers['Content-Type'] = 'application/json'
+        resp.headers['Provided-By'] = "https://www.payfort.com"
         resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
-
-# @app.endpoint('api.bins')
-# def bins():
-#     private = request.form.get('private') in ['true', 'on']
-#     name = request.form.get('name')
-#     name = re.sub('[^A-Za-z0-9]+', '', name)
-#
-#     if name == '':
-#         name = tinyid()
-#     else:
-#         name = name[0:20]
-#
-#     if db.bin_exist(name):
-#         flash('Duplicate inspector name!')
-#         return redirect("/")
-#     else:
-#         bin = db.create_bin(private, name)
-#         if bin.private:
-#             session[bin.name] = bin.secret_key
-#         return _response(bin.to_dict())
 
 
 @app.endpoint('api.deletebin')
@@ -106,6 +88,7 @@ def deletebin():
     db.delete_bin(name)
     render_template('home.html', recent=expand_recent_bins())
     return redirect("/")
+
 
 @app.endpoint('api.bin')
 def bin(name):
@@ -127,7 +110,6 @@ def requests(bin):
     return _response([r.to_dict() for r in bin.requests])
 
 
-
 @app.endpoint('api.request')
 def request_(bin, ref):
     try:
@@ -146,12 +128,10 @@ def request_(bin, ref):
         else:
             for k in req.query_string:
                 if ref in req.query_string[k]:
-                        json_query = req.query_string
-                        return _safe_query_response(json_query)
+                    json_query = req.query_string
+                    return _safe_query_response(json_query)
 
     return _response({'error': "Request not found"}, 404)
-
-
 
 
 @app.endpoint('api.stats')
@@ -173,4 +153,3 @@ def inspectors():
     resp = make_response(json.dumps(inspectors), 200)
     resp.headers['Content-Type'] = 'application/json'
     return resp
-
