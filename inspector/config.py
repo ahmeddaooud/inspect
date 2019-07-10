7f378d1ae3fe8182a0fb25fe3c41be10c1f626aa
ahmeddaooud <33223701+ahmeddaooud@users.noreply.github.com>
2019-07-10 09:40:41 +0300

    Update config.py

--- inspector/config.py
+++ inspector/config.py
@@ -16,7 +16,7 @@ BIN_TTL = 2*168*3600
 STORAGE_BACKEND = "inspector.storage.memory.MemoryStorage"
 MAX_RAW_SIZE = int(os.environ.get('MAX_RAW_SIZE', 1024*10))
 IGNORE_HEADERS = ["X-Via", "Via", "X-Varnish", "X-Heroku-Dynos-In-Use", "X-Heroku-Queue-Depth", "X-Request-Start", "X-Forwarded-Port", "X-Request-Id", "X-Forwarded-Proto", "X-Forwarded-For", "Content-Length", "Accept-Encoding", "Connection", "Accept", "Cache-Control", "Postman-Token", "User-Agent", "Accept-Language", "Upgrade-Insecure-Requests", "Cookie", "Host"]
-MAX_REQUESTS = 2000
+MAX_REQUESTS = 500
 CLEANUP_INTERVAL = 12*3600
 
 REDIS_URL = ""
