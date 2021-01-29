d368b50d3e06efe45a28fbff4a68e4417ad00bae
ahmeddaooud <33223701+ahmeddaooud@users.noreply.github.com>
2021-01-29 12:29:49 +0200

    Update api.py

--- inspector/api.py
+++ inspector/api.py
@@ -21,6 +21,60 @@ def _response(object, code=200):
     return resp
 
 
+def _safe_form_response(object, code=200):
+    jsonp = request.args.get('jsonp')
+    if jsonp:
+        resp = make_response('%s(%s)' % (jsonp, json.dumps(object)), 200)
+        resp.headers['Content-Type'] = 'text/javascript'
+    else:
+        # FORMAT FORM
+        json_format = json.dumps(object, sort_keys=True, indent=4, separators=(',', ': '))
+        json_format = json_format.replace('=', '\": \"')
+        json_format = json_format.replace('&', '\", \"')
+        json_format = json_format.replace('+', ' ')
+        json_format = urllib2.unquote(json_format)
+        json_format = json_format.replace('\\', '\\\\')
+        json_format = json_format.replace('\"{', '{')
+        json_format = json_format.replace('}\"', '}')
+        json_format = '{' + json_format + '}'
+        resp = make_response(json_format, code)
+        resp.headers['Content-Type'] = 'application/json'
+        resp.headers['Provided-By'] = "https://www.payfort.com"
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+
+def _safe_query_response(object, code=200):
+    jsonp = request.args.get('jsonp')
+    if jsonp:
+        resp = make_response('%s(%s)' % (jsonp, json.dumps(object)), 200)
+        resp.headers['Content-Type'] = 'text/javascript'
+    else:
+        # FORMAT query
+        json_format = json.dumps(object)
+        resp = make_response(json_format, code)
+        resp.headers['Content-Type'] = 'application/json'
+        resp.headers['Provided-By'] = "https://www.payfort.com"
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+
+def _safe_json_response(object, code=200):
+    jsonp = request.args.get('jsonp')
+    if jsonp:
+        resp = make_response('%s(%s)' % (jsonp, json.dumps(object)), 200)
+        resp.headers['Content-Type'] = 'text/javascript'
+    else:
+        # FORMAT JSON
+        json_format = json.dumps(object, sort_keys=True, indent=4, separators=(',', ': '))
+        json_safe_format = yaml.safe_load(json_format)
+        resp = make_response(json_safe_format, code)
+        resp.headers['Content-Type'] = 'application/json'
+        resp.headers['Provided-By'] = "https://www.payfort.com"
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+
 @app.endpoint('api.deletebin')
 def deletebin():
     name = request.form['name']
@@ -34,6 +88,7 @@ def deletebin():
     return redirect("/")
 
 
+
 @app.endpoint('api.bin')
 def bin(name):
     block = ['AutoTransactionUrl']
@@ -42,16 +97,46 @@ def bin(name):
     else:
         return _response({'error': "Inspector not blocked"}, 200)
 
-        return _response(bin.to_dict())
-
 
 @app.endpoint('api.requests')
 def requests(bin):
     try:
         bin = db.lookup_bin(bin)
     except KeyError:
-        return _response({'msg': "done"}, 200)
+        return _response({'error': "Inspector not found"}, 206)
+
     return _response([r.to_dict() for r in bin.requests])
+
+
+@app.endpoint('api.alljson')
+def request_(bin):
+    try:
+        bin = db.lookup_bin(bin)
+    except KeyError:
+        return _response({'error': "Inspector not found"}, 206)
+
+@app.endpoint('api.alljson')
+def request_(bin):
+    try:
+        bin = db.lookup_bin(bin)
+    except KeyError:
+        return _response({'error': "Inspector not found"}, 206)
+    return _response([convert_to_json(req) for req in bin.requests])
+
+
+def convert_to_json(req):
+    if req.body != "":
+            json_body = req.body
+            return _safe_json_response(json_body).json
+    elif req.form_data != [] and 'application/x-www-form-urlencoded' in req.content_type:
+        if req.raw != "":
+            json_raw = req.raw
+            return _safe_form_response(json_raw).json
+    else:
+        for k in req.query_string:
+            if req.query_string[k] != {}:
+                json_query = req.query_string
+                return _safe_query_response(json_query).json
 
 
 @app.endpoint('api.request')
