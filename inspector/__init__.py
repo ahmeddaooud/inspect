3b9f625882bf7403b3950478a886983197dcf2f9
ahmeddaooud <33223701+ahmeddaooud@users.noreply.github.com>
2021-02-20 11:20:08 +0200

    Update __init__.py

--- inspector/__init__.py
+++ inspector/__init__.py
@@ -1,3 +1,9 @@
+session['logged_in']
+
+{% if session['logged_in'] %}
+
+
+
 import os
 import sys
 from cStringIO import StringIO
@@ -5,63 +11,12 @@ from cStringIO import StringIO
 from flask import Flask
 from flask_cors import CORS
 
-
-from sqlalchemy import *
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from inspector import config
-
-engine = create_engine(config.DATABASE_URL, echo=True)
-Base = declarative_base()
-
 import config
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-class User(Base):
-    """"""
-    __tablename__ = "users"
 
-    id = Column(Integer)
-    name = Column(String)
-    username = Column(String, primary_key=True)
-    password = Column(String)
-    user_policy = Column(String)
-    creation_date = Column(String)
-    active = Column(Boolean)
-
-    # ----------------------------------------------------------------------
-    def __init__(self, name, username, password, user_policy, creation_date, active):
-        """"""
-        self.name = name
-        self.username = username
-        self.password = password
-        self.user_policy = user_policy
-        self.creation_date = creation_date
-        self.active = active
-
-
-# #  create tables
-        Base.metadata.create_all(engine)
-
-        engine = create_engine(config.DATABASE_URL, echo=True)
-
-# #  create a Session
-        Sessionmaker = sessionmaker(bind=engine)
-        sessionmaker = Sessionmaker()
-
-        user = User("Admin User", "admin@payfort.com", "85abcec2435819f27b76fc72eb9574d49b4d6fe19f70d96ecfbb7ca0efcf7f47", "admin", "08-02-2020", True)
-        sessionmaker.add(user)
-
-        user = User("test User", "user@payfort.com", "85abcec2435819f27b76fc72eb9574d49b4d6fe19f70d96ecfbb7ca0efcf7f47", "admin", "08-02-2020", True)
-        sessionmaker.add(user)
-
-        sessionmaker.commit()
-    
-    
 class WSGIRawBody(object):
     def __init__(self, application):
         self.application = application
